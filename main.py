c6f0f682e54dad14b8f1be9f15035d99ff3c54ff
skacholia <74026955+skacholia@users.noreply.github.com>
2023-03-14 19:18:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ pinecone.init(
 index_name = "acc-municode"
 
 @st.cache_resource
-acc_pinecone = Pinecone.from_existing_index(index_name=index_name,embedding=embeddings)
+def get_pinecone():
+    return Pinecone.from_existing_index(index_name=index_name,embedding=embeddings)
+
+acc_pinecone = get_pinecone()
 query = st.text_input("`Please ask a question about ACC's Code of Ordinances:` ","How many parking spaces does a bowling alley need")
 info = " In which section can I find this information?"
 qa = ChatVectorDBChain.from_llm(OpenAI(temperature=0, model_name="gpt-3.5-turbo"), acc_pinecone, return_source_documents=True)
